e866b0a9c4de4de585216b0d35f121591a9e0f1b
unknown <temple0715@gmail.com>
2023-02-17 07:07:55 -0600

    added buttons for calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator"></div>
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+        
+        <div>
+          <button>/</button><button>*</button>
+          <button>+</button><button>-</button>
+
+        </div>
     </div>
   );
 }
