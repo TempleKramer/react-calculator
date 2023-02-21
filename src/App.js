4c12651be1189171da3d88fcb2adb75ee1877417
unknown <temple0715@gmail.com>
2023-02-20 22:34:53 -0600

    created numbers function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 
 
 function App() {
+    const createDigits = ()  => {
+      const digits = [];
+
+     for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+     }
+     return digits
+    }
+
+
   return (
     <div className="App">
       <div className="calculator"></div>
@@ -17,6 +29,7 @@ function App() {
           <butt>DELETE</butt>
           </div>
           <div className="digits">
+            {createDigits()}
             <button>0</button>
             <button>.</button>
             <button>=</button>
